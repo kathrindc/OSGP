b3d516b46ecd2e1732536befab43e1c73053c511
Kathrin De Cecco <kad@toast.ws>
2023-04-10 21:42:21 +0200

    Web: Add managed CORS fairing for web service

--- web/src/main.rs
+++ web/src/main.rs
@@ -5,8 +5,11 @@ mod respond;
 mod schema;
 mod security;
 
+use dotenvy::dotenv;
 use handlers::{logon_handler, user_handler};
-use rocket::{get, launch, routes};
+use rocket::{get, http::Method, launch, routes};
+use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
+use std::env;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -15,18 +18,46 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount(
-        "/api",
-        routes![
-            index,
-            user_handler::list_users_handler,
-            user_handler::create_user_handler,
-            user_handler::read_user_handler,
-            user_handler::update_user_handler,
-            user_handler::change_user_password_handler,
-            user_handler::delete_user_handler,
-            logon_handler::get_logon_handler,
-            logon_handler::start_logon_handler,
-        ],
-    )
+    dotenv().ok();
+
+    let allowed_origins = AllowedOrigins::some_exact(&[
+        env::var("CORS_ORIGIN").expect("CORS_ORIGIN environment variable must be set")
+    ]);
+    let allowed_headers = AllowedHeaders::All;
+    let allowed_methods = vec![
+        Method::Get,
+        Method::Post,
+        Method::Put,
+        Method::Patch,
+        Method::Delete,
+    ]
+    .into_iter()
+    .map(From::from)
+    .collect();
+    let cors = CorsOptions {
+        allowed_origins,
+        allowed_headers,
+        allowed_methods,
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("CORS configuration is not valid");
+
+    rocket::build()
+        .mount(
+            "/api",
+            routes![
+                index,
+                user_handler::list_users_handler,
+                user_handler::create_user_handler,
+                user_handler::read_user_handler,
+                user_handler::update_user_handler,
+                user_handler::change_user_password_handler,
+                user_handler::delete_user_handler,
+                logon_handler::get_logon_handler,
+                logon_handler::start_logon_handler,
+            ],
+        )
+        .attach(cors)
 }
